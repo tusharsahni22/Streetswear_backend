c4e05d8973d440e9d48ba8749953f248355cd59e
Tushar Sahni <tusharsahni22@.com>
2024-01-04 15:50:46 +0530

    Update dependencies and connection settings

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// const client = new MongoClient(process.env.DB_URI, {useUnifiedTopology: true});
 
-
-mongoose.connect(process.env.DB_URI).then(() => {
+mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Database connection successful');
 }).catch(err => {
     console.error('Database connection error');
