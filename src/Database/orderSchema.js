0f7ec7ba2c062fd7a240f3db9ea9a2243e6ff2e2
Tushar Sahni <tusharsahni22@.com>
2024-03-03 19:17:33 +0530

    Refactor orderSchema.js to fix required field bug

--- src/Database/orderSchema.js
+++ src/Database/orderSchema.js
@@ -76,8 +76,7 @@ const orderSchema = new mongoose.Schema({
         state: {
             type: String,
             required: true
-        },
-        required: true
+        }
     },
     orderId:{
         type: String,
