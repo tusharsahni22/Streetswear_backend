3c186c1ef3a07d2183ebffa598b0c49098c0811c
Tushar Sahni <tusharsahni22@gmail.com>
2024-03-24 20:26:00 +0530

    Refactor OTP verification code to use MSG91 API

--- src/Controller/Otpverification.js
+++ src/Controller/Otpverification.js
@@ -1,24 +1,42 @@
 const OtpSchema = require("../Database/MobileOtp")
-const twilio = require('twilio');
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new twilio(accountSid, authToken);
+const http = require('http');
 
 
 const otpGenration = async (req, res) => {
     const { phone } = req.body;
     const otp = Math.floor(100000 + Math.random() * 900000);
-    console.log(otp);
     await OtpSchema.findOneAndUpdate({ phone: phone }, { otp: otp }, { upsert: true })
 
-    client.messages
-        .create({
-            body: `Your otp is ${otp}`,
-            from: process.env.TWILIO_PHONE_NUMBER,
-            to: `+91`+phone
-        })
-        .then(message => console.log(message.sid));
-    res.send("Otp successfully sent to your mobile number");
+    const options = {
+        "method": "POST",
+        "hostname": "control.msg91.com",
+        "port": null,
+        "path": `/api/v5/otp?template_id=${process.env.MSG91_TEMPLATE_ID}&mobile=${`91`+phone}&authkey=${process.env.MSG91_AUTH_KEY}&otp=${otp}&invisible=&otp_length=6`,
+        "headers": {
+          "Content-Type": "application/JSON"
+        }
+      };
+
+const httpReq = http.request(options, function (httpRes) {
+  const chunks = [];
+
+  httpRes.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  httpRes.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+    if (httpRes.statusCode === 200) {
+        res.send(body.toString()); // send the response to the client only if the status code is 200
+      } else {
+        res.status(httpRes.statusCode).send('Error occurred while sending OTP');
+      }
+  });
+});
+
+httpReq.write("{\n  \"Param1\": \"value1\",\n  \"Param2\": \"value2\",\n  \"Param3\": \"value3\"\n}");
+httpReq.end();
 }
 
 const otpVerification = async (req, res) => {
@@ -26,7 +44,6 @@ const otpVerification = async (req, res) => {
     const data = await OtpSchema.findOne({ phone: phone, otp: otp })
         if (data) {
             await OtpSchema.findOneAndDelete({ phone: phone, otp: otp }).then((data) => {
-                console.log(data);
             })
             res.send("Otp verified successfully");
         }
